dfff2c932b6f2b14c17b9cc4c304bfae0ac00214
Tim Robin <git@timrobin.net>
2020-02-24 22:39:34 +0100

    Adding Unity test project. Compile the rust part with `cargo build` and copy Rust/fluent-unity/target/debug/libfluent_unity.dylib to Unity/Test/Assets/Plugins and rename to `libfluent_unity.bundle` to make it usable

---
+++ Unity/Test/Assets/Scripts/RunTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class RunTest : MonoBehaviour
+{
+    [DllImport("libfluent_unity")]
+    private static extern int double_input(int x);
+
+    [DllImport("libfluent_unity")]
+    private static extern int triple_input(int x);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(double_input(14));
+        Debug.Log(triple_input(14));
+    }
+}
